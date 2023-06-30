4d9c7b5a61d704ace73dcc85f62590c078d0f2c5
dnniranjan <dnalininiranjan@hotmail.com>
2023-06-30 07:25:22 +0000

    mofified

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const Notification = (props) => {
-    {content,url,contClass}=props;
-  <div className={contClass}>
-    <img
-      className="image"
-      src={url}
-    />
-    <p className="text">{content}</p>
-  </div>;
+  const { content, url, contClass } = props;
+
+  return (
+    <div className={contClass}>
+      <img className="image" src={url} />
+      <p className="text">{content}</p>
+    </div>
+  );
 };
 
 const element = (
@@ -14,11 +14,26 @@ const element = (
     <div className="main">
       <h1 className="heading">Notifications</h1>
       <div>
-      <Notification content="Information Message" url="https://assets.ccbp.in/frontend/react-js/primary-icon-img.png" contClass="information"/>
-      <Notification content="Success Message" url="https://assets.ccbp.in/frontend/react-js/success-icon-img.png" contClass="success"/>
-      <Notification content="Warning Message" url="https://assets.ccbp.in/frontend/react-js/warning-icon-img.png" contClass="warning"/>
-      <Notification content="Danger Message" url="https://assets.ccbp.in/frontend/react-js/danger-icon-img.png" contClass="danger"/>> 
-        
+        <Notification
+          content="Information Message"
+          url="https://assets.ccbp.in/frontend/react-js/primary-icon-img.png"
+          contClass="information"
+        />
+        <Notification
+          content="Success Message"
+          url="https://assets.ccbp.in/frontend/react-js/success-icon-img.png"
+          contClass="success"
+        />
+        <Notification
+          content="Warning Message"
+          url="https://assets.ccbp.in/frontend/react-js/warning-icon-img.png"
+          contClass="warning"
+        />
+        <Notification
+          content="Danger Message"
+          url="https://assets.ccbp.in/frontend/react-js/danger-icon-img.png"
+          contClass="danger"
+        />
       </div>
     </div>
   </div>
